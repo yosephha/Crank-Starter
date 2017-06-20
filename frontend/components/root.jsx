bb257b43b13041d667974f7beb5bcafb8f4200ce
Yoseph <yosephha@buffalo.edu>
2017-06-20 17:35:56 -0400

    Prepare for initial Heroku push

--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { HashRouter } from 'react-router-dom';
-import App from './App';
+import App from './app';
 
 const Root = ({ store }) => (
   <Provider store={ store }>
