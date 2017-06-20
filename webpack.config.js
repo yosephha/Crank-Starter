1dcdafed51b789bb31d2aa42e0d9e4661f5e48a8
Yoseph <yosephha@buffalo.edu>
2017-06-19 20:53:00 -0400

    Setup

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = {
+  context: __dirname,
+  entry: './frontend/crankStarter.jsx',
+  output: {
+    path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
+    filename: 'bundle.js'
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '*']
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015']
+        }
+      }
+    ]
+  },
+  devtool: 'source-maps'
+};
