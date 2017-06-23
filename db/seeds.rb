3403b0ed6ccecf049d5ac417c7b0817c523e5b1b
Yoseph <yosephha@buffalo.edu>
2017-06-23 16:36:41 -0400

    added destroy_all in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,12 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
+User.destroy_all
 # Users
 user1 = User.create(username: 'user', password: 'password')
 user2 = User.create(username: 'yoseph', password: 'password')
 user3 = User.create(username: 'steveJobs', password: 'password')
 
+Category.destroy_all
 #categories
 art = Category.create(name: 'Art')
 game = Category.create(name: 'Game')
@@ -18,6 +19,7 @@ music = Category.create(name: 'Music')
 tech = Category.create(name: 'Technology')
 photography = Category.create(name: 'Photography')
 
+Project.destroy_all
 #Projects
 p1 = Project.create( title: 'Open a Shop', website: 'www.shop.com',
     description: 'bla bla blabla bla blabla bla blabla bla bla bla blabla blbla
@@ -53,6 +55,7 @@ p4 = Project.create( title: 'Fund a Startup', website: 'www.startup.com',
     funding_goal: '40000000', details: 'bla bla bla',
     creator_id: user3.id, category_id: photography.id)
 
+Reward.destroy_all
 # Rewards
 r1 = Reward.create(amount: 10, title: 'pizza Lunch',
      description: 'bla...bla..bla...bla...bla..bl',
@@ -67,8 +70,8 @@ r4 = Reward.create(amount: 10000, title: 'bigg hug',
      description: 'bla...bla..bla...bla...bla..bl',
      project_id: p4.id)
 
+Contribution.destroy_all
 # contributions
-
 c1 = Contribution.create(reward_id: r1.id, backer_id: user1.id, amount: 2)
 c2 = Contribution.create(reward_id: r2.id, backer_id: user2.id, amount: 1)
 c3 = Contribution.create(reward_id: r3.id, backer_id: user3.id, amount: 5)
