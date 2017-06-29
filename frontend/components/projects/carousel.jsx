ca237cf5b7702f7b175898fa4d221218c163ec52
Yoseph <yosephha@buffalo.edu>
2017-06-29 17:35:33 -0400

    Styled the splash

--- frontend/components/projects/carousel.jsx
+++ frontend/components/projects/carousel.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import Carousel from "nuka-carousel";
-import carouselArrows from "./decorators";
+import Decorators from "./decorators";
 
 const nukaCarousel = () => (
-  <Carousel wrapAround={true} autoplay={true} autoplayInterval={10000} decorators={carouselArrows}>
+  <Carousel wrapAround={true} autoplay={true} autoplayInterval={10000} decorators={Decorators}>
     <img src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide1"/>
     <img src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide1"/>
     <img src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide1"/>
--- frontend/components/projects/decorators.jsx
+++ frontend/components/projects/decorators.jsx
@@ -1,42 +1,121 @@
-import React from 'react';
+import React from "react";
 
-export default [
+const Decorators = [
   {
     component: React.createClass({
-        render() {
-            return (
-              <div
-                  style={this.styleArrow(this.props.currentSlide === 0)}
-                  onClick={this.props.previousSlide}>
-                  Previous Slide
-              </div>
-            );
-        },
-
-        shouldComponentUpdate() {
-           return this.props.currentSlide === 0;
-         },
-
-        styleArrow(disabled) {
-           return { opacity: disabled ? 0 : 1 };
-         }
-    })
+      render() {
+        return (
+          <button
+            style={this.getButtonStyles(this.props.currentSlide === 0 && !this.props.wrapAround)} >
+            <img src="https://www.shareicon.net/data/64x64/2015/10/18/658096_bracket_512x512.png"
+              id="leftButton" onClick={this.handleClick} />
+          </button>
+        );
+      },
+      handleClick(e) {
+        e.preventDefault();
+        this.props.previousSlide();
+      },
+      getButtonStyles(disabled) {
+        return {
+          border: 0,
+          background: 'rgba(0,0,0,0)',
+          color: 'white',
+          padding: 10,
+          outline: 0,
+          opacity: disabled ? 0.3 : 1,
+        };
+      }
+    }),
+    position: 'CenterLeft'
   },
-
   {
     component: React.createClass({
-        render() {
-            console.log('rendering arrow right');
-            return (
-                <div
-                    style={this.styleArrow(this.props.currentSlide === this.props.slideCount - 1)}
-                    onClick={this.props.nextSlide}>
-                    Next Slide
-                </div>
-            );
-        },
-        shouldComponentUpdate() { return this.props.currentSlide === this.props.slideCount - 1; },
-        styleArrow(disabled) { return { opacity: disabled ? 0 : 1 }; }
-    })
+      render() {
+        return (
+          <button
+            style={this.getButtonStyles(this.props.currentSlide + this.props.slidesToScroll >= this.props.slideCount && !this.props.wrapAround)} >
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/8a/OCR-A_char_Greater-Than_Sign.svg/848px-OCR-A_char_Greater-Than_Sign.svg.png"
+              id="rightButton" onClick={this.handleClick} />
+          </button>
+        );
+      },
+      handleClick(e) {
+        e.preventDefault();
+        this.props.nextSlide();
+      },
+      getButtonStyles(disabled) {
+        return {
+          border: 0,
+          background: 'rgba(0,0,0,0)',
+          color: 'white',
+          padding: 10,
+          outline: 0,
+          opacity: disabled ? 0.3 : 1,
+        };
+      }
+    }),
+    position: 'CenterRight'
+  },
+  {
+    component: React.createClass({
+      render() {
+        var self = this;
+        var indexes = this.getIndexes(self.props.slideCount, self.props.slidesToScroll);
+        return (
+          <ul style={self.getListStyles()}>
+            {
+              indexes.map(function(index) {
+                return (
+                  <li style={self.getListItemStyles()} key={index}>
+                    <button
+                      style={self.getButtonStyles(self.props.currentSlide === index)}
+                      onClick={self.props.goToSlide.bind(null, index)}>
+                      &bull;
+                    </button>
+                  </li>
+                )
+              })
+            }
+          </ul>
+        );
+      },
+      getIndexes(count, inc) {
+        var arr = [];
+        for (var i = 0; i < count; i += inc) {
+          arr.push(i);
+        }
+        return arr;
+      },
+      getListStyles() {
+        return {
+          position: 'relative',
+          margin: 0,
+          top: -10,
+          padding: 0
+        };
+      },
+      getListItemStyles() {
+        return {
+          listStyleType: 'none',
+          display: 'inline-block'
+        }
+      },
+      getButtonStyles(active) {
+        return {
+          border: 0,
+          background: 'transparent',
+          color: 'black',
+          cursor: 'pointer',
+          padding: 10,
+          outline: 0,
+          fontSize: 24,
+          opacity: active ? 1 : 0.5
+        };
+      }
+    }),
+    position: 'BottomCenter'
   }
 ];
+
+export default Decorators;
