a9329c886cba2cf8086524645acb1399fd66c53e
Yoseph <yosephha@buffalo.edu>
2017-07-04 14:41:08 -0400

    fix

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,7 @@ module CrankStarter
     # -- all .rb files in that directory are automatically loaded.
     config.paperclip_defaults = {
       :storage => :s3,
+      :s3_protocol => :https,
       :s3_credentials => {
         :bucket => ENV["s3_bucket"],
         :access_key_id => ENV["s3_access_key_id"],
