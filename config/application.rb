2a3afd125edbdc63ceae5d3edc9b786931ad7d1c
Yoseph <yosephha@buffalo.edu>
2017-07-04 13:36:16 -0400

    fix

--- config/application.rb
+++ config/application.rb
@@ -17,7 +17,9 @@ module CrankStarter
         :bucket => ENV["s3_bucket"],
         :access_key_id => ENV["s3_access_key_id"],
         :secret_access_key => ENV["s3_secret_access_key"],
-        :s3_region => ENV["s3_region"]
+        :s3_region => ENV["s3_region"],
+        s3_host_name: "s3-#{ENV["s3_region"]}.amazonaws.com",
+        url: ":s3_host_name"
       }
     }
   end
