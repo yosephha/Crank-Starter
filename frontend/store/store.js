5808f657f9d5cfa56e43519ffd78d3b4a5b9848b
Yoseph <yosephha@buffalo.edu>
2017-07-03 21:43:23 -0400

    Remove minification warning

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,13 +1,19 @@
 import { createStore, applyMiddleware } from 'redux';
 import RootReducer from '../reducers/root_reducer';
-import thunk from 'redux-thunk'
-import logger from 'redux-logger';
+import thunk from 'redux-thunk';
+
+const middlewares = [thunk];
+
+if (process.env.NODE_ENV !== 'production') {
+  const { logger } = require('redux-logger');
+  middlewares.push(logger);
+}
 
 const configureStore = (preloadedState = {}) => (
   createStore(
     RootReducer,
     preloadedState,
-    applyMiddleware(thunk, logger)
+    applyMiddleware(...middlewares)
   )
 )
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,25 @@
 const path = require('path');
+const webpack = require("webpack");
+
+let plugins = [];
+const devPlugins = [];
+
+const prodPlugins = [
+  new webpack.DefinePlugin({
+    'process.env': {
+      'NODE_ENV': JSON.stringify('production')
+    }
+  }),
+  new webpack.optimize.UglifyJsPlugin({
+    compress: {
+      warnings: true
+    }
+  })
+];
+
+plugins = plugins.concat(
+  process.env.NODE_ENV === 'production' ? prodPlugins : devPlugins
+)
 
 module.exports = {
   context: __dirname,
@@ -7,6 +28,7 @@ module.exports = {
     path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
     filename: 'bundle.js'
   },
+  plugins: plugins,
   resolve: {
     extensions: ['.js', '.jsx', '*']
   },
