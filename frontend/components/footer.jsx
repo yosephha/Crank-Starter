2f7bb7e24b78bf7c79c20bf8c8955edf4646fbd0
Yoseph <yosephha@buffalo.edu>
2017-07-23 20:33:22 -0400

    Added a footer

--- frontend/components/footer.jsx
+++ frontend/components/footer.jsx
@@ -1,8 +1,40 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Footer = () => (
   <div className='footer-container'>
-    <a href="https://github.com/yosephha">Github</a>
+    <div className='inner-footer-container'>
+      <div>
+        <Link to="/" className="nav-bar-logo site-nav__item footer-logo-container">
+          <span id="logo">
+            <span className="footer-logo">Crank</span>
+            <span className="crank">starter</span>
+          </span>
+          <br />
+        </Link>
+        <div className="cpr-txt">Copyright © 2017 Crankstarter. All rights reserved.</div>
+      </div>
+
+      <ul>
+        <li>
+          <a target="_blank" href="https://github.com/yosephha" className="icon fa-linkdin">
+          <i className="devicon-github-plain"></i>
+          </a>
+        </li>
+
+        <li>
+          <a target="_blank" href="https://www.linkedin.com/in/yoseph-habtewold-b7625881" className="linkedin-link">
+          <i className="fa fa-linkedin" aria-hidden="true"></i>
+          </a>
+        </li>
+
+        <li>
+          <a target="_blank" href="mailto:yoseph.habtewold@gmail.com">
+            <i className="fa fa-envelope-o" aria-hidden="true"></i>
+          </a>
+        </li>
+      </ul>
+    </div>
   </div>
 );
 
