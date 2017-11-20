62ca5a6a5e3d5d0206095c05fa4cf70133fc8702
Yoseph <yosephha@buffalo.edu>
2017-11-20 10:44:22 -0500

    fixed indent

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -2,21 +2,21 @@
 class Api::SessionsController < ApplicationController
 
   def create
-  	@user = User.find_by_credentials(
+    @user = User.find_by_credentials(
       params[:user][:username],
       params[:user][:password]
     )
 
-    if @user
-			login(@user)
-			render "api/users/show"
-		else
-			render (
+    if @users
+      login(@user)
+      render "api/users/show"
+    else
+      render(
         json: ["Invalid Credentials"],
         status: 401
       )
-		end
-	end
+    end
+  end
 
 	def destroy
 		@user = current_user
