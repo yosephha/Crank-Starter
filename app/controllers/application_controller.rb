68d3d4e3559e21fd35e0b1a9639f40821e53eafe
Yoseph <yosephha@buffalo.edu>
2017-11-20 16:28:28 -0500

    Fixed typo

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,7 +20,7 @@ class ApplicationController < ActionController::Base
 
   def logout
     current_user.reset_session_token!
-    session[:session_token]
+    session[:session_token] = nil
     @current_user = nil
   end
 
