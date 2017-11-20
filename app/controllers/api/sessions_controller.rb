d1ccf84d4580bc4f81b7ba8d6df65329b142ab3f
Yoseph <yosephha@buffalo.edu>
2017-11-20 10:47:03 -0500

    fixed indent

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -18,16 +18,17 @@ class Api::SessionsController < ApplicationController
     end
   end
 
-	def destroy
-		@user = current_user
-		if @user
-			logout
-			render "api/users/show"
-		else
-			render(
+  def destroy
+    @user = current_user
+
+    if @user
+      logout
+      render "api/users/show"
+    else
+      render(
         json: ["Nobody signed in"],
         status: 404
       )
-		end
-	end
+    end
+  end 
 end
